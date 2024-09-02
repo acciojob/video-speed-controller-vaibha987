7525a31755fa4d39b39d94ff167eb1826a3ff236
vaibha987 <v2goyal95@gmail.com>
2024-09-02 21:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('#player');
+const toggle = document.querySelector('#toggle');
+const volume = document.querySelector('#volume');
+const playbackSpeed = document.querySelector('#playbackSpeed');
+const rewind = document.querySelector('#rewind');
+const forward = document.querySelector('#forward');
+const progress = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+rewind.addEventListener('click', skip);
+forward.addEventListener('click', skip);
+
+volume.addEventListener('change', handleRangeUpdate);
+playbackSpeed.addEventListener('change', handleRangeUpdate);
